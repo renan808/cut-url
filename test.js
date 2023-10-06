8372d8cfe69d039b602d9019d187a81824ed6b19
Pedro Renan <129890874+renan808@users.noreply.github.com>
2023-10-06 15:01:42 -0300

    Some updates

--- test.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios').default
-
-axios.request("https://www.yeaaawdasaxafssdf44").then((test) => {
-    console.log(test.status)
-}).catch((err) => {
-    console.log(err.code)
-})
